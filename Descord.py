4764189e5768c43dc25eb072e74d1839a06211ce
zzzzzz31 <naruto057x@gmail.com>
2026-08-09 06:30:38 +0300

    Update Descord.py

--- Descord.py
+++ Descord.py
@@ -2,7 +2,7 @@ import asyncio
 import io
 import logging
 import os
-from datetime import datetime, timezone
+from datetime import datetime, timezoneA
 from zoneinfo import ZoneInfo
 
 import aiohttp
